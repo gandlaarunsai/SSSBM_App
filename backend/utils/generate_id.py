14c5e11673a8ebf79c1062fa45407c5aaeb0814b
gandlaarunsai <100899324+gandlaarunsai@users.noreply.github.com>
2025-07-17 17:46:19 +0530

    Update generate_id.py
    Added a feature that can generate an ID for both Edonation and feedback with different identification codes.

--- backend/utils/generate_id.py
+++ backend/utils/generate_id.py
@@ -2,14 +2,16 @@ import datetime
 import secrets
 import string
 
-# Function to create a 30 character Unique ID to used for data base storage
-def generate_id(length=8):
+def generate_id(length=8, idFor='edonation'):
     
     timestamp= datetime.datetime.utcnow().strftime('%f%d%m%Y%H%M%S')
 
     alphabet = string.ascii_uppercase + string.digits
     unique_part = ''.join(secrets.choice(alphabet) for _ in range(length))
 
-    id= f"{timestamp}ED{unique_part}"
+    if idFor == 'feedback':
+        id = f"{timestamp}FD{unique_part}"
+    else:
+        id= f"{timestamp}ED{unique_part}"
     
     return id
