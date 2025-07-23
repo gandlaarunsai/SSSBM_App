c9339dc5ac8438ea00d1d5a7f5102645c6b26acc
gandlaarunDev <gandlaarunsai.dev@gmail.com>
2025-07-23 18:02:05 +0530

    Updated main tag styles.

--- frontend/src/pages/RootLayout.jsx
+++ frontend/src/pages/RootLayout.jsx
@@ -4,13 +4,20 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/footer UI/Footer";
 
 export default function RootLayout() {
+
+    const mainStyle = {
+        padding: "4rem",
+        "max-width": "94.4rem",
+        "align-self": "center"
+    };
+
     return (
         <>
             <header>
                 <Header />
                 <Navbar />
             </header>
-            <main>
+            <main style={mainStyle}>
                 <Outlet />
             </main>
             <Footer />
