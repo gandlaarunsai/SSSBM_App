df21068f62abf085545a288f1b9362427656eccb
Arun Sai G <gandlaarunsai@gmail.com>
2025-09-20 00:04:53 +0530

    updated styles for tablet and mobile

--- frontend/src/pages/RootLayout.jsx
+++ frontend/src/pages/RootLayout.jsx
@@ -7,13 +7,7 @@ import Navbar from "../components/Header UI/Navbar";
 import Footer from "../components/footer UI/Footer";
 
 export default function RootLayout() {
-
-    // const mainStyle = {
-    //     padding: "4rem",
-    //     // "max-width": "94.4rem",
-    //     "align-self": "center"
-    // };
-
+    
     return (
         <>
             <header>
