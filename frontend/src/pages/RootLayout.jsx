673a769cafd250b6284eaed56c167147c3d84dd9
Arun Sai G <gandlaarunsai@gmail.com>
2025-09-19 11:56:24 +0530

    updated edonation styles for tablet

--- frontend/src/pages/RootLayout.jsx
+++ frontend/src/pages/RootLayout.jsx
@@ -1,15 +1,18 @@
 import { Outlet } from "react-router-dom";
+
+import classes from './RootLayout.module.css';
+
 import Header from "../components/Header UI/Header";
 import Navbar from "../components/Header UI/Navbar";
 import Footer from "../components/footer UI/Footer";
 
 export default function RootLayout() {
 
-    const mainStyle = {
-        padding: "4rem",
-        // "max-width": "94.4rem",
-        "align-self": "center"
-    };
+    // const mainStyle = {
+    //     padding: "4rem",
+    //     // "max-width": "94.4rem",
+    //     "align-self": "center"
+    // };
 
     return (
         <>
@@ -17,7 +20,7 @@ export default function RootLayout() {
                 <Header />
                 <Navbar />
             </header>
-            <main style={mainStyle}>
+            <main className={classes.rootLayout_main}>
                 <Outlet />
             </main>
             <Footer />
